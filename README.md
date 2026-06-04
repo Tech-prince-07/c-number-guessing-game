272c4057f8595a3e812210564d20d4006e83783d
Tech-prince-07 <256259313+Tech-prince-07@users.noreply.github.com>
2026-06-04 12:18:47 +0530

    The guess_number file has been successfully added.

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-# c-number-guessing-game
+# c-number-guessing-
+<br>
 A collection of beginner-level C programming project a number guessing game. This project demonstrate fundamental concepts like loops, conditionals, functions, and user input handling in C.
+<br>
+Author - Prince
---
+++ guess_number.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+void compare(int, int);
+void compare(int random_number, int guessed_number)
+{
+    if (guessed_number > random_number)
+    {
+        printf("Guessed number is greater than actual number\n");
+    }
+    else if (guessed_number < random_number)
+    {
+        printf("Guessed number is smaller than actual number\n");
+    }
+    else if (guessed_number == random_number)
+    {
+        printf("You guessed correct number\n");
+    }
+    else
+        printf("You type invalid number! please enter integer from 1 to 100\n");
+}
+
+int main()
+{
+    int random_number, guessed_number, attempt; // count wala mujhe samjh nhi aaya
+    // Seed the random number generator
+    srand(time(NULL)); // // har bar run krne pe random number aayega
+
+    // Generate random number between 1 and 100
+    random_number = (rand() % 100) + 1;
+
+    printf("\nGuess the number between 1 to 100:");
+    scanf("%d", &guessed_number);
+
+    printf("Your guessed number is : %d\n", guessed_number);
+    compare(random_number, guessed_number);
+
+    while (guessed_number != random_number)
+    {
+
+        printf("\nGuess the number again between 1 to 100:");
+        scanf("%d", &guessed_number);
+
+        printf("\nYour guessed number is : %d\n", guessed_number);
+        compare(random_number, guessed_number);
+    };
+
+    return 0;
+}
